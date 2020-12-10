fa324f45a8c1eae2ac5a2eda09cfef9b63e08826
Ajay <ajaykumarak1114@gmail.com>
2020-12-10 15:41:03 +0530

    Add event listener for buttons

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+for(var i=0; i<document.getElementsByClassName("drum").length; i++)
+{
+  document.getElementsByClassName("drum")[i].addEventListener("click", handleClick);
+}
 
+function handleClick()
+{
+  alert("I got clicked");
+}
