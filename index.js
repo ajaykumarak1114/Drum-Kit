84adbcec19f4f23e66011824fcd69d7392c11694
Ajay <ajaykumarak1114@gmail.com>
2020-12-10 18:17:19 +0530

    Different sounds add for the different buttons

--- index.js
+++ index.js
@@ -6,6 +6,44 @@ for(var i=0; i<document.getElementsByClassName("drum").length; i++)
 function handleClick()
 {
   this.style.color="white";
-  var audio = new Audio("sounds/tom-1.mp3");
-  audio.play();
+  switch (this.innerHTML) {
+    case "w" :
+        var tom1 = new Audio("sounds/tom-1.mp3");
+        tom1.play();
+      break;
+
+    case "a" :
+        var tom2 = new Audio("sounds/tom-2.mp3");
+        tom2.play();
+      break;
+
+    case "s" :
+        var tom3 = new Audio("sounds/tom-3.mp3");
+        tom3.play();
+      break;
+
+    case "d" :
+        var tom4 = new Audio("sounds/tom-4.mp3");
+        tom4.play();
+      break;
+
+      case "j" :
+          var crack = new Audio("sounds/crash.mp3");
+          crack.play();
+        break;
+
+      case "k" :
+          var kick = new Audio("sounds/kick-bass.mp3");
+          kick.play();
+        break;
+
+      case "l" :
+          var snare = new Audio("sounds/snare.mp3");
+          snare.play();
+        break;
+
+    default:
+
+  }
+
 }
